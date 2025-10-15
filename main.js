c1ec6b5999451c71c340f53e43c1af37963e579f
marzia <you@example.com>
2025-10-15 17:20:10 +0200

    add snack 3

--- main.js
+++ main.js
@@ -1,3 +1,49 @@
+/** // Snack 1 - Filtra e Modifica 
+
+// Crea un array (longBooks) con i libri che hanno più di 300 pagine;
+
+const longBooks = books.filter((books) => {
+  return books.pages > 300;
+});
+
+console.log(longBooks);
+
+// Creare un array (longBooksTitles) che contiene solo i titoli dei libri contenuti in longBooks.
+
+const longBooksTitles = longBooks.map((longBooks) => {
+  return longBooks.title;
+});
+// Stampa in console ogni titolo nella console.
+
+console.log(longBooksTitles);
+*/
+
+/** 
+// Snack 2 - Il primo libro scontato
+// Creare un array (availableBooks) che contiene tutti i libri disponibili.
+
+const availableBooks = books.filter((books) => {
+  return books.available === true;
+});
+
+//console.log(availableBooks);
+
+// Crea un array (discountedBooks) con gli availableBooks, ciascuno con il prezzo scontato del 20% (mantieni lo stesso formato e arrotonda al centesimo)
+
+const discountedBooks = availableBooks.map((availableBooks) => {
+  const number = Number(availableBooks.price.replace("€", "")) * 0.8;
+  return { ...availableBooks, price: number.toFixed(2) + "€" };
+});
+
+// Salva in una variabile (fullPricedBook) il primo elemento di discountedBooks che ha un prezzo intero (senza centesimi).
+
+const fullPricedBook = discountedBooks.find((b) => {
+  const number = Number(b.price.replace("€", ""));
+  return Number.isInteger(number);
+});
+
+console.log(fullPricedBook);*/
+
 const books = [
   {
     title: "React Billionaire",
@@ -45,42 +91,21 @@ const books = [
   },
 ];
 
-/** // Snack 1 - Filtra e Modifica 
+// Snack 3 - Ordinare gli Autori
+// Creare un array (authors) che contiene gli autori dei libri.
 
-// Crea un array (longBooks) con i libri che hanno più di 300 pagine;
+const authors = books.map((books) => books.author);
+console.log(authors);
 
-const longBooks = books.filter((books) => {
-  return books.pages > 300;
-});
+// Crea una variabile booleana (areAuthorsAdults) per verificare se gli autori sono tutti maggiorenni.
 
-console.log(longBooks);
+const areAuthorsAdults = authors.every((a) => a.age > 18);
 
-// Creare un array (longBooksTitles) che contiene solo i titoli dei libri contenuti in longBooks.
+//false
+console.log(areAuthorsAdults);
 
-const longBooksTitles = longBooks.map((longBooks) => {
-  return longBooks.title;
-});
-// Stampa in console ogni titolo nella console.
+// Ordina l’array authors in base all’età, senza creare un nuovo array. (se areAuthorsAdult è true, ordina in ordine crescente, altrimenti in ordine decrescente)
 
-console.log(longBooksTitles);
-*/
+authors.sort((a, b) => b.age - a.age);
 
-// Snack 2 - Il primo libro scontato
-// Creare un array (availableBooks) che contiene tutti i libri disponibili.
-
-const availableBooks = books.filter((books) => {
-  return books.available === true;
-});
-
-console.log(availableBooks);
-
-// Crea un array (discountedBooks) con gli availableBooks, ciascuno con il prezzo scontato del 20% (mantieni lo stesso formato e arrotonda al centesimo)
-
-const discountedBooks = availableBooks.map((availableBooks) => {
-  const number = Number(availableBooks.price.replace("€", "")) * 0.8;
-  return { ...availableBooks, price: number.toFixed(2) + "€" };
-});
-
-// Salva in una variabile (fullPricedBook) il primo elemento di discountedBooks che ha un prezzo intero (senza centesimi).
-
-console.log(discountedBooks);
+console.log(authors);
