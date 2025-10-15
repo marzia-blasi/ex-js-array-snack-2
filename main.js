fdd8c197abad1010e73eb43dd07ab1dcda7db6f8
marzia <you@example.com>
2025-10-15 22:55:34 +0200

    refactor: reorganize book data structure and enhance longBooks functionality

--- main.js
+++ main.js
@@ -1,3 +1,50 @@
+const books = [
+  {
+    title: "React Billionaire",
+    pages: 250,
+    author: {
+      name: "Alice",
+      age: 35,
+    },
+    available: false,
+    price: "101€",
+    tags: ["advanced", "js", "react", "senior"],
+  },
+  {
+    title: "Advanced JS",
+    pages: 500,
+    author: {
+      name: "Bob",
+      age: 20,
+    },
+    available: true,
+    price: "25€",
+    tags: ["advanced", "js", "mid-senior"],
+  },
+  {
+    title: "CSS Secrets",
+    pages: 320,
+    author: {
+      name: "Alice",
+      age: 17,
+    },
+    available: true,
+    price: "8€",
+    tags: ["html", "css", "junior"],
+  },
+  {
+    title: "HTML Mastery",
+    pages: 200,
+    author: {
+      name: "Charlie",
+      age: 50,
+    },
+    available: false,
+    price: "48€",
+    tags: ["html", "advanced", "junior", "mid-senior"],
+  },
+];
+
 /** // Snack 1 - Filtra e Modifica 
 
 // Crea un array (longBooks) con i libri che hanno più di 300 pagine;
@@ -16,6 +63,9 @@ const longBooksTitles = longBooks.map((longBooks) => {
 // Stampa in console ogni titolo nella console.
 
 console.log(longBooksTitles);
+
+longBooksTitles.forEach(t => console.log(t)); 
+
 */
 
 /** 
@@ -63,53 +113,6 @@ console.log(areAuthorsAdults);
 authors.sort((a, b) => b.age - a.age);
 
 console.log(authors);*/
-
-const books = [
-  {
-    title: "React Billionaire",
-    pages: 250,
-    author: {
-      name: "Alice",
-      age: 35,
-    },
-    available: false,
-    price: "101€",
-    tags: ["advanced", "js", "react", "senior"],
-  },
-  {
-    title: "Advanced JS",
-    pages: 500,
-    author: {
-      name: "Bob",
-      age: 20,
-    },
-    available: true,
-    price: "25€",
-    tags: ["advanced", "js", "mid-senior"],
-  },
-  {
-    title: "CSS Secrets",
-    pages: 320,
-    author: {
-      name: "Alice",
-      age: 17,
-    },
-    available: true,
-    price: "8€",
-    tags: ["html", "css", "junior"],
-  },
-  {
-    title: "HTML Mastery",
-    pages: 200,
-    author: {
-      name: "Charlie",
-      age: 50,
-    },
-    available: false,
-    price: "48€",
-    tags: ["html", "advanced", "junior", "mid-senior"],
-  },
-];
 
 // Snack 4 - Calcola l’età media
 
